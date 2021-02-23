ad035c4bec370d8d118e1912ea84e8c011b89c38
Vijayganesh3107 <64351388+Vijayganesh3107@users.noreply.github.com>
2021-02-23 05:56:24 -0500

    Final

--- js/blog.js
+++ js/blog.js
@@ -3,3 +3,13 @@ var btn=document.querySelector(".projbtn");
 btn.addEventListener("click",()=>{
     location.href="../html/Projects.html"
 })
+
+
+var notebookbtn=document.querySelector(".notebookbtn");
+var kaggleknowbtn=document.querySelector(".kaggleknowbtn");
+notebookbtn.addEventListener("click",()=>{
+    location.href="https://www.kaggle.com/vijayganesh3107/vijay-assignemnt-v2"
+})
+kaggleknowbtn.addEventListener("click",()=>{
+    location.href="../html/Blog.html"
+})
