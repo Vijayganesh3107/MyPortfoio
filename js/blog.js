485d70d9ec873cb183967c07be6e717537d38bda
Vijayganesh3107 <64351388+Vijayganesh3107@users.noreply.github.com>
2021-03-30 05:27:18 -0400

    Cifar-10 Blog

--- js/blog.js
+++ js/blog.js
@@ -1,14 +1,23 @@
 var btn=document.querySelector(".projbtn");
+var cifarbtn=document.querySelector(".cifarknowbtn");
+var cifarnotebookbtn=document.querySelector(".cifarnotebookbtn");
 
 btn.addEventListener("click",()=>{
     location.href="../html/Projects.html"
 })
+cifarbtn.addEventListener("click",()=>{
+    location.href="../html/Cifar10_Blog.html"
+})
+
 
 
 var notebookbtn=document.querySelector(".notebookbtn");
 var kaggleknowbtn=document.querySelector(".kaggleknowbtn");
 notebookbtn.addEventListener("click",()=>{
     location.href="https://www.kaggle.com/vijayganesh3107/vijay-assignemnt-v2"
+})
+cifarnotebookbtn.addEventListener("click",()=>{
+    location.href="https://colab.research.google.com/drive/147kT6-1zz8oaDD4NS24Qd_0OfLMRk1NF?usp=sharing"
 })
 kaggleknowbtn.addEventListener("click",()=>{
     location.href="../html/Blog.html"
