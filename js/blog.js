32825b6ad7330598213dec2ea84c7c9a6b68ffc0
Vijayganesh3107 <64351388+Vijayganesh3107@users.noreply.github.com>
2021-04-27 13:31:36 -0400

    Final Push

--- js/blog.js
+++ js/blog.js
@@ -3,6 +3,7 @@ var cifarbtn=document.querySelector(".cifarknowbtn");
 var overfittingknowbtn=document.querySelector(".overfittingknowbtn");
 var cifarnotebookbtn=document.querySelector(".cifarnotebookbtn");
 var overfittingnotebookbtn=document.querySelector(".overfittingnotebookbtn");
+var sentianalknowbtn=document.querySelector(".sentianalknowbtn");
 
 btn.addEventListener("click",()=>{
     location.href="../html/Projects.html"
@@ -13,11 +14,16 @@ cifarbtn.addEventListener("click",()=>{
 overfittingknowbtn.addEventListener("click",()=>{
     location.href="../html/Overfitting_Blog.html"
 })
+sentianalknowbtn.addEventListener("click",()=>{
+    location.href="../html/Sentiment_Analysis.html"
+})
 
 
 
 var notebookbtn=document.querySelector(".notebookbtn");
 var kaggleknowbtn=document.querySelector(".kaggleknowbtn");
+var sentianalnotebookbtn=document.querySelector(".sentianalnotebookbtn");
+
 notebookbtn.addEventListener("click",()=>{
     location.href="https://www.kaggle.com/vijayganesh3107/vijay-assignemnt-v2"
 })
@@ -26,6 +32,9 @@ cifarnotebookbtn.addEventListener("click",()=>{
 })
 overfittingnotebookbtn.addEventListener("click",()=>{
     location.href="https://colab.research.google.com/drive/1tzdyfUPxZ3XxEBDJTW4ELbhFxfv87pRQ?usp=sharing"
+})
+sentianalnotebookbtn.addEventListener("click",()=>{
+    location.href="https://www.kaggle.com/vijay420/sentiment-analysis"
 })
 kaggleknowbtn.addEventListener("click",()=>{
     location.href="../html/Blog.html"
